56f877725bd46b0dd3161f2cc5e68ae74ea36c72
Richard Staub <rgstaub@gmail.com>
2019-02-04 22:20:17 -0500

    Set up styled-components to work with server side rendering. Set up Nprogress bar. Did some basic example styling ing the header, page, and theme

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,43 @@
 import Link from 'next/link';
+import styled from 'styled-components';
+import Router from 'next/router';
+import NProgress from 'nprogress'
 import Nav from './Nav.js';
+
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+}
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+}
+Router.onRouteChangeError = () => {
+  NProgress.done();
+}
+
+const Icon = styled.a`
+  cursor: pointer;
+  display: inline-block;
+  height: 100px;
+  margin: 1rem;
+  box-shadow: 1px 1px 5px #999;
+  border-radius: 3px;
+  @media (min-width: 600px) {
+    height: 80px;
+  
+  }
+  img {
+    height: 100%;
+  }
+`
 
 const Header = () => {
   return (
     <div>
       <Link href="/">
-        <a>THL</a>
+        <Icon>
+          <img src="/static/card-small.png" alt="THL" />
+        </Icon>
       </Link>
       <Nav />
     </div>
--- components/Page.js
+++ components/Page.js
@@ -4,15 +4,42 @@ import Header from '../components/Header.js';
 import Meta from '../components/Meta.js';
 
 
+
 const theme = {
   maxWidth: '1200px',
-
+  fontFamily:  '\'Encode Sans Condensed\', sans-serif',
+  brightBlue: '#14d0ff'
 }
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Encode+Sans+Condensed:200,400,700');
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0px;
+    margin: 0px;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: ${theme.fontFamily};
+  }
+`
+
+const StyledPage = styled.div`
+  h1 {
+    font-weight: 200;
+  }
+`
 
 const Inner = styled.div`
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
+  
 `
 
 class Page extends Component {
@@ -21,15 +48,14 @@ class Page extends Component {
   render() { 
     return (
       <ThemeProvider theme={theme}>
-        <div>
+        <StyledPage>
           <Meta />
           <Header />
           <Inner>
             {this.props.children}
           </Inner>
-        </div>
+        </StyledPage>
       </ThemeProvider>
-
     );
   }
 }
